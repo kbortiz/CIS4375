be54c1adffd04aa2866a3549dd330fece632e314
kimln1996 <kimluong596@gmail.com>
2023-10-23 12:02:24 -0500

    complete search table and sorting table

--- search_box.js
+++ search_box.js
@@ -29,39 +29,62 @@ function searchTable() {
 
 
 // Sorting
-table_headings.forEach((head, i) => {
-    let sort_asc = true;
-    head.onclick = () => {
-        table_headings.forEach(head => head.classList.remove('active'));
-        head.classList.add('active');
+    let sortColumn = null;
+    let sortAsc = true;
 
-        document.querySelectorAll('td').forEach(td => td.classList.remove('active'));
-        table_rows.forEach(row => {
-            row.querySelectorAll('td')[i].classList.add('active');
-        })
+    // Add click event listeners to table headings for sorting
+    table_headings.forEach((head, i) => {
+        head.addEventListener('click', () => {
+            // Toggle active class for sorting indication
+            table_headings.forEach(h => h.classList.remove('active'));
+            head.classList.add('active');
+    
+            // Toggle sorting order
+            if (i === sortColumn) {
+                sortAsc = !sortAsc;
+            } else {
+                sortColumn = i;
+                sortAsc = true;
+            }
+    
+            sortTable(i, sortAsc);
+        });
+    });
 
-        head.classList.toggle('asc', sort_asc);
-        sort_asc = head.classList.contains('asc') ? false : true;
-
-        sortTable(i, sort_asc);
+    function sortTable(column, sort_asc) {
+        const sortedRows = [...table_rows].sort((a, b) => {
+            const first_cell = a.querySelectorAll('td')[column];
+            const second_cell = b.querySelectorAll('td')[column];
+            const first_value = first_cell.textContent.trim();
+            const second_value = second_cell.textContent.trim();
+    
+            if (column === 3) {
+                // Handle date values in "MM/DD/YYYY" format
+                const firstDateParts = first_value.split('/');
+                const secondDateParts = second_value.split('/');
+                const firstDate = new Date(firstDateParts[2], firstDateParts[0] - 1, firstDateParts[1]);
+                const secondDate = new Date(secondDateParts[2], secondDateParts[0] - 1, secondDateParts[1]);
+                return sort_asc ? firstDate - secondDate : secondDate - firstDate;
+            } else if (!isNaN(first_value) && !isNaN(second_value)) {
+                // Handle numeric values
+                return sort_asc ? first_value - second_value : second_value - first_value;
+            } else {
+                // Fallback to string comparison for other columns
+                return sort_asc ? first_value.localeCompare(second_value) : second_value.localeCompare(first_value);
+            }
+        });
+    
+        const tbody = document.querySelector('.content-table tbody');
+        tbody.innerHTML = '';
+        sortedRows.forEach(sorted_row => {
+            tbody.appendChild(sorted_row);
+        });
     }
-})
-
-
-function sortTable(column, sort_asc) {
-    const sortedRows = [...table_rows].sort((a, b) => {
-        let first_row = a.querySelectorAll('td')[column].textContent.toLowerCase();
-        let second_row = b.querySelectorAll('td')[column].textContent.toLowerCase();
-
-        return sort_asc ? (first_row < second_row ? 1 : -1) : (first_row < second_row ? -1 : 1);
-    });
-
-    const tbody = document.querySelector('tbody');
-    tbody.innerHTML = ''; // Clear the tbody content
-
-    sortedRows.forEach(sorted_row => {
-        tbody.appendChild(sorted_row); // Append each sorted row to the tbody
-    });
-}
+    
+    
+    function isDate(value) {
+        // Check if a string is in a date format (you might need to adjust this based on your date format)
+        return /^\d{4}-\d{2}-\d{2}$/.test(value);
+    }
 
 });
