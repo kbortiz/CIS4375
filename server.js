13e7ecb5e91d027085b52bb3aed5b1863bbd6118
Joseph Nguyen <77763346+jaqdnguyen@users.noreply.github.com>
2023-10-23 01:51:23 -0500

    Update server.js to add routes to delete reviews and customer info

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 // Define a middleware to set the currentUrl for all routes
 app.use((req, res, next) => {
@@ -11,16 +12,78 @@ app.use((req, res, next) => {
     next();
 });
 
+
 app.get('/', (req, res) => {
     res.render('check-in');
 });
 
+
 app.get('/reward', (req, res) => {
-    res.render('reward');
+    const customers = [
+        {
+            firstName: 'Peter',
+            lastName: 'Parks',
+            phoneNumber: '8321121113',
+            lastVisit: '10/10/2023',
+            currentPoints: 2,
+            lifetimePoints: 5
+        },
+        {
+            firstName: 'Kim',
+            lastName: 'Luong',
+            phoneNumber: '2819127693',
+            lastVisit: '10/20/2023',
+            currentPoints: 10,
+            lifetimePoints: 50
+        },
+        {
+            firstName: 'Willy',
+            lastName: 'Lanka',
+            phoneNumber: '2819225555',
+            lastVisit: '10/2/2023',
+            currentPoints: 4,
+            lifetimePoints: 4
+        },
+    ];
+    res.render('reward', { customers });
 });
 
 app.get('/reviews', (req, res) => {
     res.render('reviews');
+});
+
+app.get('/delete-reviews', (req, res) => {
+    res.render('delete-reviews');
+});
+
+app.get('/customer-information', (req, res) => {
+    const customers = [
+        {
+            firstName: 'Peter',
+            lastName: 'Parks',
+            phoneNumber: '8321121113',
+            emailAddress: 'peterpark@gmail.com',
+            birthday: '1/1/2000',
+            lastVisit: '10/10/2023'
+        },
+        {
+            firstName: 'Kim',
+            lastName: 'Luong',
+            phoneNumber: '2819127693',
+            emailAddress: 'kimluong@gmail.com',
+            birthday: '2/14/1999',
+            lastVisit: '10/20/2023'
+        },
+        {
+            firstName: 'Willy',
+            lastName: 'Lanka',
+            phoneNumber: '2819225555',
+            emailAddress: 'willylanka@gmail.com',
+            birthday: '7/22/2002',
+            lastVisit: '10/2/2023'
+        },
+    ];
+    res.render('customer-information', { customers });
 });
 
 app.listen(port, () => {
