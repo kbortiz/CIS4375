5f08515e03e8854429c02f165fde791190050a9c
kimln1996 <kimluong596@gmail.com>
2023-10-29 23:07:08 -0500

    fixing error edited data does not make change to server data

--- server.js
+++ server.js
@@ -6,6 +6,37 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.static(__dirname));
 
+app.use(express.json());
+
+const customers = [
+    {
+        firstName: 'Peter',
+        lastName: 'Parks',
+        phoneNumber: '8321121113',
+        lastVisit: '10/10/2023',
+        currentPoints: 3,
+        lifetimePoints: 5
+    },
+    {
+        firstName: 'Kim',
+        lastName: 'Luong',
+        phoneNumber: '2819127693',
+        lastVisit: '10/20/2023',
+        currentPoints: 10,
+        lifetimePoints: 50
+    },
+    {
+        firstName: 'Willy',
+        lastName: 'Lanka',
+        phoneNumber: '2819225555',
+        lastVisit: '10/2/2023',
+        currentPoints: 4,
+        lifetimePoints: 4
+    },
+];
+
+
+
 // Define a middleware to set the currentUrl for all routes
 app.use((req, res, next) => {
     res.locals.currentUrl = req.originalUrl;
@@ -19,32 +50,6 @@ app.get('/', (req, res) => {
 
 
 app.get('/reward', (req, res) => {
-    const customers = [
-        {
-            firstName: 'Peter',
-            lastName: 'Parks',
-            phoneNumber: '8321121113',
-            lastVisit: '10/10/2023',
-            currentPoints: 3,
-            lifetimePoints: 5
-        },
-        {
-            firstName: 'Kim',
-            lastName: 'Luong',
-            phoneNumber: '2819127693',
-            lastVisit: '10/20/2023',
-            currentPoints: 10,
-            lifetimePoints: 50
-        },
-        {
-            firstName: 'Willy',
-            lastName: 'Lanka',
-            phoneNumber: '2819225555',
-            lastVisit: '10/2/2023',
-            currentPoints: 4,
-            lifetimePoints: 4
-        },
-    ];
     res.render('reward', { customers });
 });
 
@@ -111,6 +116,20 @@ app.get('/redemption-history', (req, res) => {
 });
 
 
+app.put('/reward/updateCurrentPoints/:phoneNumber', (req, res) => {
+    const phoneNumber = req.params.phoneNumber;
+    const newPoints = req.body.newPoints;
+
+    // Find and update the customer in the 'customers' array
+    const customer = customers.find((c) => c.phoneNumber === phoneNumber);
+    if (customer) {
+        customer.currentPoints = newPoints;
+        res.json({ success: true });
+    } else {
+        console.error('Customer not found for phone number: ' + phoneNumber);
+        res.status(404).json({ error: 'Customer not found' });
+    }
+});
 
 
 app.listen(port, () => {
