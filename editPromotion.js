0f656454a4e33866c6188bc1bcc4d93e7e44ec69
kimln1996 <kimluong596@gmail.com>
2023-10-30 20:30:27 -0500

    fixing error other cells beside status not editable when click edit

--- editPromotion.js
+++ editPromotion.js
@@ -1,10 +1,24 @@
-    // Edit promotion 
+
+    // Function to edit a promotion
     function editPromotion(id) {
         const row = document.querySelector(`tr[data-promo-id="${id}"]`);
+        const fields = row.querySelectorAll('td[data-field]');
+
+        fields.forEach(field => {
+            const currentValue = field.textContent.trim();
+
+            if (field.getAttribute('data-field') !== 'promo_status') {
+                // Create an input element for editing other fields
+                const input = document.createElement('input');
+                input.value = currentValue;
+                field.innerHTML = '';
+                field.appendChild(input);
+            }
+        });
+
+        // Create a dropdown with options for ACTIVE and INACTIVE for the "Status" field
         const statusField = row.querySelector('td[data-field="promo_status"]');
         const currentStatus = statusField.textContent.trim();
-
-        // Create a dropdown with options for ACTIVE and INACTIVE
         const dropdown = document.createElement('select');
         dropdown.className = 'status-edit';
         
@@ -26,11 +40,11 @@
             inactiveOption.selected = true;
         }
 
-        // Replace the content with the dropdown
+        // Replace the content of the "Status" field with the dropdown
         statusField.innerHTML = '';
         statusField.appendChild(dropdown);
 
-        // Change "Edit" button to "Save" button with green color
+        // Change the button text to "Save"
         const editButton = row.querySelector('.edit-button');
         editButton.textContent = 'Save';
         editButton.style.backgroundColor = '#40a347';
@@ -38,78 +52,64 @@
         editButton.onclick = () => savePromotion(id);
     }
 
+    // Function to save a promotion
     function savePromotion(id) {
-    const row = document.querySelector(`tr[data-promo-id="${id}"]`);
-    const fields = row.querySelectorAll('td');
+        const row = document.querySelector(`tr[data-promo-id="${id}"]`);
+        const fields = row.querySelectorAll('td[data-field]');
 
-    // Collect the updated values from the editable fields
-    const updatedValues = {};
-    fields.forEach(field => {
-        updatedValues[field.getAttribute('data-field')] = field.innerText;
-        field.contentEditable = false;
-    });
+        // Create an object to hold the updated values
+        const updatedValues = {};
 
-    // Add the updated status to updatedValues
-    const statusField = row.querySelector('td[data-field="promo_status"]');
-    const statusValue = statusField.querySelector('select').value;
-    updatedValues['promo_status'] = statusValue;
+        fields.forEach(field => {
+            const fieldName = field.getAttribute('data-field');
+            
+            if (fieldName === 'promo_status') {
+                // Handle the "Status" field separately
+                const statusField = row.querySelector('td[data-field="promo_status"]');
+                const statusValue = statusField.querySelector('select').value;
+                updatedValues[fieldName] = statusValue;
+            } else {
+                // Handle other fields
+                const input = field.querySelector('input');
+                updatedValues[fieldName] = input.value;
+                field.innerHTML = input.value;
+            }
+        });
 
-    // Send updatedValues to the server via an AJAX request for saving
-    fetch(`/promotion/updatePromotion/${id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updatedValues),
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Handle the response, e.g., show success or error messages
-        if (data.success) {
-            // Data was successfully updated on the server
-            // Update the status in the UI
-            const statusField = row.querySelector('td[data-field="promo_status"]');
-            const statusValue = data.promo_status; // Assuming the response includes the updated status
-            statusField.innerHTML = `<div class="status ${statusValue}">${statusValue}</div>`;
-        } else {
-            // Handle the error
-        }
-    })
-    .catch(error => {
-        // Handle the fetch error
-    });
+        // Send the updated values to the server via an AJAX request for saving
+        fetch(`/promotion/updatePromotion/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedValues),
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                // Update the UI with the saved values
+                fields.forEach(field => {
+                    const fieldName = field.getAttribute('data-field');
+                    
+                    if (fieldName !== 'promo_status') {
+                        field.innerHTML = updatedValues[fieldName];
+                    }
+                });
 
-    // Change "Save" button back to "Edit" button
-    const saveButton = row.querySelector('.edit-button');
-    saveButton.textContent = 'Edit';
-    saveButton.style.backgroundColor = ''; // Remove the green color
-    saveButton.onclick = () => editPromotion(id);
-}
+                // Update the "Status" field
+                const statusField = row.querySelector('td[data-field="promo_status"]');
+                statusField.innerHTML = `<div class="status ${updatedValues.promo_status}">${updatedValues.promo_status}</div>`;
+            } else {
+                // Handle the error
+            }
+        })
+        .catch(error => {
+            // Handle the fetch error
+        });
 
-    document.getElementById('submitPromotion').addEventListener('click', function() {
-    // Get the form data
-    const pointsCost = document.querySelector('[name="pointsCost"]').value;
-    const expirationDate = document.querySelector('[name="expirationDate"]').value;
-    const description = document.querySelector('[name="promotionName"]').value; // Changed to "promotionName"
-    const status = document.querySelector('[name="status"]').checked ? 'ACTIVE' : 'INACTIVE';
-
-    // Create a new table row
-    const newRow = document.createElement('tr');
-    newRow.innerHTML = `
-        <td>New ID</td>
-        <td>${description}</td>
-        <td>${description}</td>
-        <td>${pointsCost}</td>
-        <td>${expirationDate}</td>
-        <td class="${status}">${status}</td>
-        <td><button class="edit-button" onclick="editPromotion('New ID')">Edit</button></td>
-    `;
-
-    // Append the new row to the table
-    document.querySelector('.content-table tbody').appendChild(newRow);
-
-    // After adding a new promotion, you should add an event listener for editing that new promotion.
-    newRow.querySelector('.edit-button').addEventListener('click', function() {
-        editPromotion('New ID'); // You can use a unique ID for the new promotion
-    });
-});
+        // Change the button text back to "Edit"
+        const saveButton = row.querySelector('.edit-button');
+        saveButton.textContent = 'Edit';
+        saveButton.style.backgroundColor = '';
+        saveButton.onclick = () => editPromotion(id);
+    }
