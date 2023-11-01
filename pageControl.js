8f6c03a41ec2c23f5cb1352d12a08cd3d19bea7d
kimln1996 <kimluong596@gmail.com>
2023-10-31 19:23:36 -0500

    fixing pagination to work on all pages

--- pageControl.js
+++ pageControl.js
@@ -1,58 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Control amount of items on a single page
-    let currentPage = 1;
-    const totalItems = document.getElementById('totalCustomers').textContent;
-    const itemsPerPage = 25; // Specify the number of items per page
-    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    // Function to initialize pagination for a specific page
+    function initializePagination(totalItems, itemsPerPage, prevButtonId, nextButtonId, currentPageId) {
+        let currentPage = 1;
+        const totalPages = Math.ceil(totalItems / itemsPerPage);
 
-    const prevPageButton = document.getElementById('prevPageButton');
-    const nextPageButton = document.getElementById('nextPageButton');
-    const currentPageElement = document.getElementById('currentPage');
+        const prevPageButton = document.getElementById(prevButtonId);
+        const nextPageButton = document.getElementById(nextButtonId);
+        const currentPageElement = document.getElementById(currentPageId);
 
-    function updateTable() {
-        const start = (currentPage - 1) * itemsPerPage;
-        const end = start + itemsPerPage;
+        function updateTable() {
+            const start = (currentPage - 1) * itemsPerPage;
+            const end = start + itemsPerPage;
 
-        const rows = document.querySelectorAll('.content-table tbody tr');
-        rows.forEach((row, index) => {
-            if (index >= start && index < end) {
-                row.style.display = 'table-row';
-            } else {
-                row.style.display = 'none';
+            const rows = document.querySelectorAll('.content-table tbody tr');
+            rows.forEach((row, index) => {
+                if (index >= start && index < end) {
+                    row.style.display = 'table-row';
+                } else {
+                    row.style.display = 'none';
+                }
+            });
+
+            prevPageButton.disabled = currentPage === 1;
+            nextPageButton.disabled = currentPage === totalPages;
+            updateCurrentPage();
+        }
+
+        function updatePaginationButtons() {
+            prevPageButton.disabled = currentPage === 1;
+            nextPageButton.disabled = currentPage === totalPages;
+        }
+
+        function updateCurrentPage() {
+            currentPageElement.textContent = currentPage;
+        }
+
+        // Attach click event listeners to pagination buttons
+        prevPageButton.addEventListener('click', () => {
+            if (currentPage > 1) {
+                currentPage--;
+                updateTable();
+                updatePaginationButtons();
             }
         });
 
-        prevPageButton.disabled = currentPage === 1;
-        nextPageButton.disabled = currentPage === totalPages;
+        nextPageButton.addEventListener('click', () => {
+            if (currentPage < totalPages) {
+                currentPage++;
+                updateTable();
+                updatePaginationButtons();
+            }
+        });
+
+        // Initial update of the current page number
         updateCurrentPage();
     }
 
-    function updatePaginationButtons() {
-        prevPageButton.disabled = currentPage === 1;
-        nextPageButton.disabled = currentPage === totalPages;
-    }
-
-    function updateCurrentPage() {
-        currentPageElement.textContent = currentPage;
-    }
-
-    // Attach click event listeners to pagination buttons
-    prevPageButton.addEventListener('click', () => {
-        if (currentPage > 1) {
-            currentPage--;
-            updateTable();
-            updatePaginationButtons();
-        }
-    });
-
-    nextPageButton.addEventListener('click', () => {
-        if (currentPage < totalPages) {
-            currentPage++;
-            updateTable();
-            updatePaginationButtons();
-        }
-    });
-
-    // Initial update of the current page number
-    updateCurrentPage();
+    // Example usage on a specific page
+    // Replace the parameters with actual values for your specific page
+    initializePagination(100, 25, 'prevPageButton', 'nextPageButton', 'currentPage');
 });
