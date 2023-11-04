e9597ebef336c5fc777570ff56e86189c90fcf0d
kpatel330 <krishnalipatel1@gmail.com>
2023-11-04 11:50:21 -0500

    redemption history 2

--- redemption.py
+++ redemption.py
@@ -6,57 +6,52 @@ from sql import execute_query
 from sql import execute_read_query
 import datetime
 from datetime import date
+app = flask(__name__)
 
-# creating connection to mysql database
-conn = create_connection("cis4375project.cpbp75z8fnop.us-east-2.rds.amazonaws.com", "admin", "password", "Davi_Nails")
+def create_connection():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host="localhost", 
+            user="root",     
+            passwd="your_password",  
+            database="MyDatabase"
+        )
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
 
-# setting up an application name
-app = flask.Flask(__name__)  # sets up the application
-app.config["DEBUG"] = True  # allow to show errors in browser
+@app.route('/redeempromotion', methods=['POST'])
+def redeem_promotion():
+    conn = create_connection("cis4375project.cpbp75z8fnop.us-east-2.rds.amazonaws.com", "admin", "password", "Davi_Nails")
+    
+    request_data = request.get_json()
+    customer_phone = request_data['phone_number']
+    promotion_name = request_data['promo_name']
 
-today = date.today()  # sets today as today's date
+    # Get customer ID based on phone number
+    get_customer_id_sql = "SELECT cust_ID FROM customer_information WHERE phone_number = %s"
+    customer_id = execute_read_query(conn, get_customer_id_sql, (customer_phone,))
 
-authorizedusers = [
-    {  # Creates user with username admin and password cars2000
-        # admin user
-        'username': 'admin',
-        'password': 'password',
-        'role': 'admin',
-        'token': '1234657890',
-        'admininfo': 'something super secret nobody can know'
-    },
-]
-@app.route('/activepromos', methods=['GET'])  # Endpoint to return currently active promotions
-def api_get_activepromos():
-    conn = create_connection("cis4375project.cpbp75z8fnop.us-east-2.rds.amazonaws.com", "admin", "password",
-                             "Davi_Nails")
-    sql = "SELECT * FROM promotions WHERE promo_status = 'Active' ORDER BY promo_cost"
-    printlogs = execute_read_query(conn, sql)
+    # Get promotion ID based on promotion name
+    get_promotion_id_sql = "SELECT promo_ID FROM promotions WHERE promo_name = %s"
+    promotion_id = execute_read_query(conn, get_promotion_id_sql, (promotion_name,))
 
-    return jsonify(printlogs)  # Prints all logs
-
-@app.route('/allpromos', methods=['GET'])  # Endpoint to return all promotions
-def api_get_allpromos():
-    conn = create_connection("cis4375project.cpbp75z8fnop.us-east-2.rds.amazonaws.com", "admin", "password",
-                             "Davi_Nails")
-    sql = "SELECT * FROM promotions ORDER BY promo_cost"
-    printlogs = execute_read_query(conn, sql)
-
-    return jsonify(printlogs)  # Prints all logs
-@app.route('/addpromotion', methods=['POST'])  # endpoint to submit a promotion
-def post_create_promo():
-    conn = create_connection("cis4375project.cpbp75z8fnop.us-east-2.rds.amazonaws.com", "admin", "password",
-                             "Davi_Nails")
-
-    request_data = request.get_json()  # provids json inputs for the needed data to be inputted
-    promoname = request_data['promo_name']
-    promodescription = request_data['promo_description']
-    promostatus = request_data['promo_status']
-    promocost = request_data['promo_cost']
-    addreview = "INSERT INTO promotions (promo_name, promo_description, promo_status, promo_cost) VALUES ('%s', '%s', '%s',%s)" % (
-    promoname, promodescription, promostatus, promocost)
-    execute_query(conn, addreview)  # executes above query to add the provided data to table
-    return 'Promotion Added'
-
-
-app.run()
+    if customer_id and promotion_id:
+       
+        add_redemption_sql = "INSERT INTO redemption_history (customer_id, promotion_id, redemption_date) VALUES (%s, %s, %s)"
+        execute_query(conn, add_redemption_sql, (customer_id[0]['cust_ID'], promotion_id[0]['promo_ID'], redemption_date))
+        return 'Promotion Redeemed successfully!'
+    else:
+        return 'Customer or Promotion not found. Please check the provided information.'
+    
+@app.route('/redemptionhistory', methods=['GET'])
+def get_redemption_history():
+    conn = create_connection("cis4375project.cpbp75z8fnop.us-east-2.rds.amazonaws.com", "admin", "password", "Davi_Nails")
+    sql = "SELECT rh.redemption_id, ci.first_name, ci.last_name, p.promo_name, rh.redemption_date " \
+        "FROM redemption_history rh " \
+        "JOIN customer_information ci ON rh.customer_id = ci.cust_ID " \
+        "JOIN promotions p ON rh.promotion_id = p.promo_ID " \
+        "ORDER BY rh.redemption_date DESC"
+    redemption_history = execute_read_query(conn, sql)
+    return jsonify(redemption_history)   
